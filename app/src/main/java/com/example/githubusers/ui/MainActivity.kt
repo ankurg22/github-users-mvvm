3162dbac53ac896c9db7dbb7ba8b15e0baf9b6c6
Ankur Gupta <guptaankur.gupta0@gmail.com>
2019-05-19 20:38:08 +0530

    Added about me option in menu

--- app/src/main/java/com/example/githubusers/ui/MainActivity.kt
+++ app/src/main/java/com/example/githubusers/ui/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.githubusers.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import com.example.githubusers.Constants
 import com.example.githubusers.R
 import com.example.githubusers.ui.userlist.UserListFragment
@@ -17,7 +21,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportFragmentManager.beginTransaction()
-                .replace(activity_content.id, UserListFragment.newInstance(Constants.MODE_ALL, ""))
-                .commit()
+            .replace(activity_content.id, UserListFragment.newInstance(Constants.MODE_ALL, ""))
+            .commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.action_about -> {
+                val intent = Intent(this, ProfileActivity::class.java)
+                intent.putExtra(Constants.KEY_USER_LOGIN, "ankurg22")
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
